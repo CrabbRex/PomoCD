3172aba2dde8c5a4e1013386ef6695f512264a9a
Rex <crabbrex@gmail.com>
2026-07-01 12:22:23 +1000

    Implement basic timer and circle

---
+++ pomo-CD/src/lib/components/Timer.svelte
@@ -0,0 +1,98 @@
+<script lang="ts">
+    import { onDestroy } from 'svelte';
+
+    let totalSeconds = 2 * 60;
+    let secondsLeft = $state(totalSeconds);
+
+    let running = $state(false);
+
+    const r = 90;
+    const c = 2 * Math.PI * r;
+
+    function progress() {
+        return secondsLeft / totalSeconds;
+    }
+
+    let interval: ReturnType<typeof setInterval> | null = null;
+
+    function startTimer() {
+        if (interval) return; // Timer is already running
+
+        running = true;
+
+        interval = setInterval(() => {
+            if (secondsLeft > 0) {
+                secondsLeft -= 1;
+            } else {
+                stop();
+            }
+        }, 1000);
+    }
+
+    function stop() {
+      running = false;
+        // if (interval) {
+        //     clearInterval(interval);
+        // }
+        // interval = null;
+    }
+
+    function reset() {
+        stop();
+        secondsLeft = totalSeconds;
+    }
+
+    function formatTime(seconds: number): string {
+        const minutes = Math.floor(seconds / 60);
+        const remainingSeconds = seconds % 60;
+        return `${minutes.toString().padStart(2, '0')}:${remainingSeconds.toString().padStart(2, '0')}`;
+    }
+
+    onDestroy(() => {
+        stop();
+    });
+</script>
+
+<div class="container">
+
+  <!-- CD -->
+  <div
+    class:animate-cd-spin={running}
+    class="relative h-72 w-72 rounded-full bg-gradient-to-br from-zinc-200 via-zinc-500 to-zinc-900 shadow-2xl"
+  >
+
+  </div>
+
+
+  <div class="text">
+    <h1>{formatTime(secondsLeft)}</h1>
+  </div>
+</div>
+<div class="timer">
+    <h1>{formatTime(secondsLeft)}</h1>
+    <button onclick={startTimer}>Start</button>
+    <button onclick={stop}>Stop</button>
+    <button onclick={reset}>Reset</button>
+</div>
+
+<!-- <style>
+  .container {
+    display: grid;
+    place-items: center;
+    position: relative;
+    height: 100vh;
+  }
+
+  .text {
+    position: absolute;
+    text-align: center;
+  }
+
+  h1 {
+    font-size: 2.5rem;
+  }
+
+  button {
+    margin: 0.5rem;
+  }
+</style> -->
--- pomo-CD/src/routes/+page.svelte
+++ pomo-CD/src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import Timer from '$lib/components/Timer.svelte';
+</script>
+
+
+
+<main class="flex bg-blue-400">
+    <h1>PomoCD</h1>
+    <Timer />
+</main>
