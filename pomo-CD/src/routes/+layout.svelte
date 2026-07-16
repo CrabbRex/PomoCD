10ba0bde1d9118edfd35ed199c1bfc3106188fb3
Rex <crabbrex@gmail.com>
2026-07-16 11:27:48 +1000

    Implement: Modal info popup. Fix: Sidedrawer sides

--- pomo-CD/src/routes/+layout.svelte
+++ pomo-CD/src/routes/+layout.svelte
@@ -4,16 +4,17 @@
 	import MusicSidebar from '$lib/components/music/MusicSidebar.svelte';
 	import SettingsPanel from '$lib/components/SettingsPanel.svelte';
 	let { children } = $props();
+	let my_modal_1: HTMLDialogElement | undefined = $state();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 {@render children()}
 
-<div class="drawer drawer-end" data-theme="luxury">
+<div class="drawer" data-theme="luxury">
 	<input id="my-drawer-1" type="checkbox" class="drawer-toggle" />
 	<div class="drawer-content">
 		<!-- Page content here -->
-		<label for="my-drawer-1" class="btn drawer-button btn-neutral top-4 right-4 fixed">
+		<label for="my-drawer-1" class="btn drawer-button btn-neutral top-4 left-4 fixed">
 			<svg
 				xmlns="http://www.w3.org/2000/svg"
 				viewBox="0 0 24 24"
@@ -38,11 +39,20 @@
 </div>
 
 <!-- Settings drawer (opens from the left) -->
-<div class="drawer drawer-end" data-theme="luxury">
+<div class="drawer" data-theme="luxury">
 	<input id="settings-drawer" type="checkbox" class="drawer-toggle" />
 	<div class="drawer-content">
-		<label for="settings-drawer" class="btn drawer-button btn-neutral top-4 right-24 fixed" aria-label="Open settings">
-			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+		<label
+			for="settings-drawer"
+			class="btn drawer-button btn-neutral top-4 left-24 fixed"
+			aria-label="Open settings"
+		>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				viewBox="0 0 24 24"
+				fill="currentColor"
+				class="size-6"
+			>
 				<path
 					fill-rule="evenodd"
 					d="M11.078 2.25c-.917 0-1.699.663-1.85 1.567L9.05 4.889c-.02.12-.115.26-.297.348a7.493 7.493 0 0 0-.986.57c-.166.115-.334.13-.45.083L6.3 5.508a1.875 1.875 0 0 0-2.282.819l-.922 1.597a1.875 1.875 0 0 0 .432 2.385l.84.692c.095.078.17.229.154.43a7.598 7.598 0 0 0 0 1.139c.015.2-.059.352-.153.43l-.841.692a1.875 1.875 0 0 0-.432 2.385l.922 1.597a1.875 1.875 0 0 0 2.282.818l1.019-.362c.115-.047.283-.032.45.083.312.214.641.405.985.57.182.088.277.228.297.35l.178 1.071c.151.904.933 1.567 1.85 1.567h1.844c.916 0 1.699-.663 1.85-1.567l.178-1.072c.02-.12.114-.26.297-.349.344-.165.673-.356.985-.57.167-.114.335-.13.45-.082l1.02.362a1.875 1.875 0 0 0 2.28-.819l.923-1.597a1.875 1.875 0 0 0-.432-2.385l-.84-.692c-.095-.078-.17-.229-.154-.43a7.614 7.614 0 0 0 0-1.139c-.016-.2.059-.352.153-.43l.84-.692c.708-.582.891-1.59.433-2.385l-.922-1.597a1.875 1.875 0 0 0-2.282-.818l-1.019.362c-.115.047-.283.032-.45-.083a7.49 7.49 0 0 0-.985-.57c-.183-.088-.277-.228-.297-.349l-.179-1.072a1.875 1.875 0 0 0-1.85-1.567h-1.843ZM12 15.75a3.75 3.75 0 1 0 0-7.5 3.75 3.75 0 0 0 0 7.5Z"
@@ -59,3 +69,26 @@
 		</div>
 	</div>
 </div>
+
+<button class="btn btn-neutral top-4 right-4 fixed" onclick={() => my_modal_1?.showModal()} data-theme="luxury" aria-label="Info Box">
+	<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+		<path
+			fill-rule="evenodd"
+			d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm8.706-1.442c1.146-.573 2.437.463 2.126 1.706l-.709 2.836.042-.02a.75.75 0 0 1 .67 1.34l-.04.022c-1.147.573-2.438-.463-2.127-1.706l.71-2.836-.042.02a.75.75 0 1 1-.671-1.34l.041-.022ZM12 9a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z"
+			clip-rule="evenodd"
+		/>
+	</svg>
+</button>
+
+<dialog bind:this={my_modal_1} class="modal" data-theme="luxury">
+	<div class="modal-box">
+		<h3 class="text-lg font-bold">PomoCD is currently a WIP</h3>
+		<p class="py-4">PomoCD takes the best of Pomodoro Timers and CD Study Music to create an environment conducive of focused study.</p>
+		<p>New features will be added as they are developed, including Spotify integration.</p>
+		<div class="modal-action">
+			<form method="dialog">
+				<button class="btn">Close</button>
+			</form>
+		</div>
+	</div>
+</dialog>
